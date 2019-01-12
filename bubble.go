ea3c1f20ed0a79c6619b943f3ae44f9c4bbef836
zhenhuanlee <zhenhuanlee@outlook.com>
2019-01-12 20:02:27 +0800

    fix

--- bubble.go
+++ bubble.go
@@ -5,9 +5,12 @@ import "fmt"
 var vec [5]int = [5]int{2, 3, 12, 0, 5}
 
 func main() {
-	for i, foo := range vec {
-		for j, too := range vec[i:] {
-			if too > foo {
+	var len = len(vec)
+
+	for i:=0; i<len; i++ {
+		for j:=i+1; j<len; j++ {
+			fmt.Println(vec[i], "-", vec[j])
+			if vec[i] > vec[j] {
 				vec[j], vec[i] = vec[i], vec[j]
 			}
 			fmt.Println(vec)
